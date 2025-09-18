3af9fd20901e2b2ba3e943ec80f9fbeedcd1083f
unknown <luan.sena1994@gmail.com>
2025-09-18 07:35:54 -0300

    init commit

---
+++ src/main/java/com/luansena/creditcardpoints/creditcardpoints/CreditcardpointsApplication.java
@@ -0,0 +1,13 @@
+package com.luansena.creditcardpoints.creditcardpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcardpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditcardpointsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luansena/creditcardpoints/creditcardpoints/CreditcardpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luansena.creditcardpoints.creditcardpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditcardpointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
